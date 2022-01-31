b8ca42f7df895c8e1d81a72d1bdbb14cc162208d
Jamezjaz <jamezjaz@gmail.com>
2022-01-31 10:06:36 +0100

    feat: Adds scholarships section

--- src/resources/js/components/home/Home.js
+++ src/resources/js/components/home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../customs/header/Header';
 import Footer from '../../customs/footer/Footer';
 import Slider from '../slider/Slider';
@@ -24,6 +25,20 @@ const Home = () => {
         <div className={homeStyles.card_sub_container}>
           <Card />
         </div>
+      </div>
+      <div id='funding' className={homeStyles.scholarships}>
+        <h2 className={homeStyles.header}>Funding Opportunities</h2>
+        <p>Fully funded programs on all academic levels. Bachelors, graduate, and, post graduate programs fully funded.</p>
+        <Link
+          to='/scholarships'
+        >
+          <button
+            type='button'
+            className='btn btn-primary'
+          >
+            Click here for details
+          </button>
+        </Link>
       </div>
       <Footer />
     </>

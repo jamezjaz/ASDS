7b4ff40099ed6534516a17eeeb72be54872dc0ad
Jamezjaz <jamezjaz@gmail.com>
2022-01-31 12:16:46 +0100

    feat: Adds newsletter section

--- src/resources/js/components/home/Home.js
+++ src/resources/js/components/home/Home.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 import Header from '../../customs/header/Header';
 import Footer from '../../customs/footer/Footer';
 import Slider from '../slider/Slider';
-import homeStyles from '../home/Home.module.css';
 import Card from '../myCard/MyCard';
+import newsletter from '../../../assets/images/newsletter.jpg';
+import homeStyles from '../home/Home.module.css';
 
 const Home = () => {
   return (
@@ -39,6 +40,28 @@ const Home = () => {
             Click here for details
           </button>
         </Link>
+      </div>
+      <div className={homeStyles.newsletter}>
+        <div>
+          <h2>Weekly Newsletter</h2>
+          <p>For updates on scholarships, research, trending posts, and more...</p>
+          <p>Please, sign up for weekly newsletter</p>
+          <div className={homeStyles.input_button}>
+            <input
+              type='email'
+              placeholder='Enter your email'
+            />
+            <button
+              type='submit'
+              className='btn btn-primary'
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+        <div className={homeStyles.news}>
+          <img src={newsletter} alt='Newsletter' />
+        </div>
       </div>
       <Footer />
     </>

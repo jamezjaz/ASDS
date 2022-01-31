8c0e71b65342f48b2ce1650bdd608456f63f7f05
Jamezjaz <jamezjaz@gmail.com>
2022-01-31 16:02:51 +0100

    feat: Implements signin page
    feat: Validates signin form

---
+++ src/resources/js/components/signin/Signin.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../../customs/header/Header';
+import signinStyles from '../signin/Signin.module.css';
+
+const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleChange = e => {
+    if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    } else {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(email, password);
+
+    if (email === '') {
+      setErrorMsg('Email is required');
+    } else if (password === '') {
+      setErrorMsg('Password is required');
+    } else {
+      setErrorMsg('');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className={signinStyles.signin_container}>
+        <div className={signinStyles.header_container}>
+          <h3>Sign in here...</h3>
+        </div>
+        {errorMsg === '' ? '' : <h5 className="text-danger text-center">{errorMsg}</h5>}
+        <div className={signinStyles.signin}>
+          <label>
+            Email:
+            <br />
+            <input
+              type='email'
+              placeholder='Enter your email'
+              name='email'
+              value={email}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <br />
+            <input
+              type='password'
+              placeholder='Enter your password'
+              name='password'
+              value={password}
+              onChange={handleChange}
+            />
+          </label>
+          <div className={signinStyles.signin_btn}>
+            <button
+              type='submit'
+              className='btn btn-primary'
+              onClick={handleSubmit}
+            >
+              Sign In
+            </button>
+          </div>
+        </div>
+        <p>
+          Don't have an account? 
+          Sign up
+          {' '}
+          <Link to='/signup'>here</Link>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Signin;
--- src/resources/js/myRoutes/MyRoutes.js
+++ src/resources/js/myRoutes/MyRoutes.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signin from '../components/signin/Signin';
 import ErrorBoundary from '../customs/errorBoundary/ErrorBoundary';
 // import Footer from '../customs/footer/Footer';
 // import Header from '../customs/header/Header';
@@ -13,7 +14,7 @@ const MyRoutes = () => {
           <Suspense fallback={<Spinner />}>
             {/* <Header /> */}
             <Routes>
-              
+              <Route path='/signin' element={<Signin />} />
             </Routes>
             {/* <Footer /> */}
           </Suspense>
